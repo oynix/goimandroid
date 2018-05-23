3ebc11ef844efeeab6772819097a3d62a683159c
xiaoyu <xiaoy@mixotc.com>
2018-05-23 17:28:21 +0800

    连接测试成功

--- app/src/main/java/com/mixotc/goimandroid/MainActivity.java
+++ app/src/main/java/com/mixotc/goimandroid/MainActivity.java
@@ -3,11 +3,15 @@ package com.mixotc.goimandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mixotc.imsdklib.connection.RemoteConnectionManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RemoteConnectionManager.getInstance().init(this);
+        RemoteConnectionManager.getInstance().connect();
     }
 }

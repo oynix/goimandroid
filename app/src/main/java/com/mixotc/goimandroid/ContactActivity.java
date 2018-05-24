2f7859ffebd7042f87a5c71b1984098a8ee365d0
xiaoyu <xiaoy@mixotc.com>
2018-05-24 14:15:02 +0800

    contact activity

---
+++ app/src/main/java/com/mixotc/goimandroid/ContactActivity.java
@@ -0,0 +1,20 @@
+package com.mixotc.goimandroid;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Author   : xiaoyu
+ * Date     : 2018/5/24 下午2:12
+ * Version  : v1.0.0
+ * Describe :
+ */
+public class ContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+    }
+}
--- app/src/main/java/com/mixotc/goimandroid/MainActivity.java
+++ app/src/main/java/com/mixotc/goimandroid/MainActivity.java
@@ -3,6 +3,7 @@ package com.mixotc.goimandroid;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Author   : xiaoyu
@@ -15,6 +16,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        View btnContact = findViewById(R.id.iv_contact);
+        btnContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }

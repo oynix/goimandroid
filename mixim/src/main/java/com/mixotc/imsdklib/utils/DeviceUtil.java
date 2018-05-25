7008929724715f4e10d6000512437e4d5979f9e9
oynix <oynix@foxmail.com>
2018-05-25 16:48:11 +0800

    mock imei

--- mixim/src/main/java/com/mixotc/imsdklib/utils/DeviceUtil.java
+++ mixim/src/main/java/com/mixotc/imsdklib/utils/DeviceUtil.java
@@ -1,7 +1,6 @@
 package com.mixotc.imsdklib.utils;
 
 import android.content.Context;
-import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 
 import static com.mixotc.imsdklib.utils.SharedPreferencesIds.KEY_IMEI;
@@ -18,16 +17,17 @@ public class DeviceUtil {
         if (context == null) {
             return "";
         }
-        String deviceId = null;
-        TelephonyManager manager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-        if (manager != null) {
-            try {
-                deviceId = manager.getDeviceId();
-            } catch (Exception e) {
-                e.printStackTrace();
-                deviceId = "";
-            }
-        }
+        // get imei of phone will fail for permission, so just mock temporary
+        String deviceId = "1213812138";
+//        TelephonyManager manager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+//        if (manager != null) {
+//            try {
+//                deviceId = manager.getDeviceId();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//                deviceId = "";
+//            }
+//        }
         if (TextUtils.isEmpty(deviceId)) {
             deviceId = SharedPreferencesUtils.getInstance(context).getString(KEY_IMEI, "");
         } else {
